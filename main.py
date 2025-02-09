8ec2ad95224bed9f01cdd6081b1c71d837f37fc6
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-02-09 13:08:43 +0200

    added cycle code with correct hit check

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from random import randint as ra
 
+from pygame.gfxdraw import pixel
+from requests.packages import target
+
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -28,6 +31,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                img_x, img_y = mouse_x - target_x, mouse_y - target_y
+                pixel = target_image.get_at((img_x, img_y))
+                if pixel.a > 0:
+                    target_x = ra(0, SCREEN_WIDTH - target_width)
+                    target_y = ra(0, SCREEN_HEIGHT - target_height)
+                    color = (ra(0, 255), ra(0, 255), ra(0, 255))
+
 
     screen.blit(target_image, (target_x, target_y))
     pygame.display.update()
