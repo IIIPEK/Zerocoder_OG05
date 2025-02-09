a46c187d8dcb9c19d6acb7ec7596d8d3319c7ae0
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-02-09 11:49:18 +0200

    add const code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ pygame.display.set_icon(icon)
 target_image = pygame.image.load("images/apple.png")
 target_width = 80
 target_height = 80
-target_x =ra(0 SCREEN_WIDTH - target_width)
+target_x =ra(0, SCREEN_WIDTH - target_width)
 target_y = ra(0, SCREEN_HEIGHT - target_height)
 
 color =(ra(0, 255), ra(0, 255), ra(0, 255))
